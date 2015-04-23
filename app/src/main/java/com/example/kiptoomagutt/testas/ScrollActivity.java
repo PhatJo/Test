46a1495ef51fd28403807fbd14a67821ddc706af
Kiptoo Magutt <kiptoo.magutt@aljazeera.net>
2015-04-23 11:55:36 +0530

    scrollable view container

--- app/src/main/java/com/example/kiptoomagutt/testas/ScrollActivity.java
+++ app/src/main/java/com/example/kiptoomagutt/testas/ScrollActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class ScrollActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scroll);
+        setContentView(R.layout.login_auth);
+
+        EditText e = (EditText)findViewById(R.id.button14);
     }
 
 
